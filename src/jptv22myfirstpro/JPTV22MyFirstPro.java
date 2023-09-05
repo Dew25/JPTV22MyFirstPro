f7c5af8e96051c7667776be2838985996029652a
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2023-09-05 10:38:33 +0300

    Первый коммит моего репозитория

---
+++ src/jptv22myfirstpro/JPTV22MyFirstPro.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv22myfirstpro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class JPTV22MyFirstPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите свое имя: ");
+        String firstname = scanner.nextLine();
+        System.out.print("Введите свою фамилию: ");
+        String lastname = scanner.nextLine();
+        
+        System.out.println("Здравствуй! " + firstname + " " + lastname );
+    }
+    
+}
