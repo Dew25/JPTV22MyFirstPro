5eb0936c7ba459beb127670e1cc09ee4b61f906c
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2023-09-14 15:05:15 +0300

    Создали класс App для управления интерфейсом пользователя программы

---
+++ src/jptv22myfirstpro/App2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv22myfirstpro;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class App2 extends App{
+
+    @Override
+    public String toString() {
+        return "App2{hashCode=" + hashCode()+ '}';
+    }
+    
+}
--- src/jptv22myfirstpro/JPTV22MyFirstPro.java
+++ src/jptv22myfirstpro/JPTV22MyFirstPro.java
@@ -17,13 +17,18 @@ public class JPTV22MyFirstPro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите свое имя: ");
-        String firstname = scanner.nextLine();
-        System.out.print("Введите свою фамилию: ");
-        String lastname = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите свое имя: ");
+//        String firstname = scanner.nextLine();
+//        System.out.print("Введите свою фамилию: ");
+//        String lastname = scanner.nextLine();
+//        
+//        System.out.println("Здравствуй! " + firstname + " " + lastname );
+
+        App app = new App();
+        app.run();
         
-        System.out.println("Здравствуй! " + firstname + " " + lastname );
+        
     }
     
 }
